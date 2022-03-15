008c9a878cec26fb824ae6f4788c1fa59bb18b13
Brianna Woodruff <briannaewoodruff@gmail.com>
2022-03-14 19:56:18 -0500

    app.js update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/NavBar/NavBar'
+import Homepage from './pages/Homepage'
+import Attachments from './pages/Attachments'
 import Login from '../src/pages/Login'
 
 function App() {
   return (
     <div className="App">
-     
-     <Login />
-     
+     {/* <Navbar /> */}
+      <Routes>
+        {/* <Route path='/' element={<Homepage />} /> */}
+        {/* <Route path='/attachment-styles' element={<Attachments />} /> */}
+        {/* <Route path='/login' element={<Login />} /> */}
+        {/* <Route path='/signup' element={<Signup />} /> */}
+      </Routes>
     </div>
   );
 }
