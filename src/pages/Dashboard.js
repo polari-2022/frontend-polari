d2b754040fce0502a97f395bc15fa1068e0e8a15
Blehjo <blehjooo@gmail.com>
2022-03-19 11:55:16 -0500

    updated dashboard login screen

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,6 +2,8 @@ import { useQuery } from '@apollo/client';
 import { QUERY_PROFILES} from '../utils/query';
 import DashboardProfiles from '../components/Dashboard/dashboardProfiles';
 
+import Auth from '../utils/auth';
+
 const Dashboard = () => {
   const { loading, data } = useQuery(QUERY_PROFILES);
 //   console.log('data', data)
@@ -9,6 +11,8 @@ const Dashboard = () => {
 
   return (
     <div>
+      {Auth.loggedIn() ? (
+      <>
         {loading ? (
         <div>Loading...</div>
         ) : (
@@ -17,6 +21,14 @@ const Dashboard = () => {
                 title="HERE are some love matches"
             />
         )}
+        </>
+      ) : (
+        <>
+          {window.location.replace('/login')}
+        </>
+      )
+      }
+
     </div>
   );
 
