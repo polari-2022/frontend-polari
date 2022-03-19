b0b73706e8dc213451c9d38edd21b43b930ed58d
Brianna Woodruff <briannaewoodruff@gmail.com>
2022-03-18 20:26:58 -0500

    navbar and login out update

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,6 +5,8 @@ import { BellIcon, MenuIcon, XIcon, ChatAltIcon } from '@heroicons/react/outline
 // routing
 import { NavLink, Link } from 'react-router-dom'
 
+import Auth from '../../utils/auth';
+
 const loggedIn = false
 
 const user = {
@@ -29,146 +31,88 @@ function classNames(...classes) {
 }
 
 export default function Navbar() {
-    if (loggedIn) {
-        return (
-            <Disclosure as="nav" className="bg-teal-900">
-                {({ open }) => (
-                    <>
-                        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                            <div className="flex justify-between h-16">
-                                <div className="flex">
-                                    <div className="-ml-2 mr-2 flex items-center md:hidden">
-                                        {/* Mobile menu button */}
-                                        <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                                            <span className="sr-only">Open main menu</span>
-                                            {open ? (
-                                                <XIcon className="block h-6 w-6" aria-hidden="true" />
-                                            ) : (
-                                                <MenuIcon className="block h-6 w-6" aria-hidden="true" />
-                                            )}
-                                        </Disclosure.Button>
-                                    </div>
-                                    <div className="flex-shrink-0 flex items-center">
-                                        <Link to='/'>
-                                            <img
-                                                className="h-20 w-auto"
-                                                src="https://user-images.githubusercontent.com/87889660/158719963-e835f37c-ef8f-4c1c-8c8b-e731aa073929.svg"
-                                                alt="Workflow"
-                                            />
-                                        </Link>
-                                    </div>
-                                    <div className="hidden md:ml-6 md:flex md:items-center md:space-x-4">
-                                        {navigation.map((item) => (
-                                            <NavLink
-                                                key={item.name}
-                                                to={item.to}
-                                                className="px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:bg-teal-700 hover:text-white focus:bg-gray-800 focus:text-white"
-                                            >
-                                                {item.name}
-                                            </NavLink>
-                                        ))}
-                                    </div>
-                                </div>
+    const logout = (event) => {
+        event.preventDefault();
+        Auth.logout();
+    };
 
-                                <div className="flex items-center">
-                                    <div className="flex-shrink-0">
-                                        <NavLink to='/messages' >
-                                            <button
-                                                type="button"
-                                                className="relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-teal-600 hover:bg-teal-500 focus:bg-teal-500 active:outline-none active:ring-2 active:ring-offset-2 active:ring-offset-gray-800 active:ring-teal-500"
-                                            >
-                                                <ChatAltIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
-                                                <span>Messages</span>
-                                            </button>
+    return (
+        <Disclosure as="nav" className="bg-teal-900">
+            {({ open }) => (
+                <>
+                    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                        <div className="flex justify-between h-16">
+                            <div className="flex">
+                                <div className="-ml-2 mr-2 flex items-center md:hidden">
+                                    {/* Mobile menu button */}
+                                    <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                                        <span className="sr-only">Open main menu</span>
+                                        {open ? (
+                                            <XIcon className="block h-6 w-6" aria-hidden="true" />
+                                        ) : (
+                                            <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                                        )}
+                                    </Disclosure.Button>
+                                </div>
+                                <div className="flex-shrink-0 flex items-center">
+                                    <Link to='/'>
+                                        <img
+                                            className="h-20 w-auto"
+                                            src="https://user-images.githubusercontent.com/87889660/158719963-e835f37c-ef8f-4c1c-8c8b-e731aa073929.svg"
+                                            alt="Workflow"
+                                        />
+                                    </Link>
+                                </div>
+                                <div className="hidden md:ml-6 md:flex md:items-center md:space-x-4">
+                                    {navigation.map((item) => (
+                                        <NavLink
+                                            key={item.name}
+                                            to={item.to}
+                                            className="px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:bg-teal-700 hover:text-white focus:bg-gray-800 focus:text-white"
+                                        >
+                                            {item.name}
                                         </NavLink>
-                                    </div>
-                                    <div className="hidden md:ml-4 md:flex-shrink-0 md:flex md:items-center">
-                                        {/* Profile */}
-                                        <Menu as="div" className="ml-3 relative">
-                                            <div>
-                                                <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                                                    <span className="sr-only">Open user menu</span>
-                                                    <img className="h-8 w-8 rounded-full" src={user.imageUrl} alt="" />
-                                                </Menu.Button>
-                                            </div>
-                                        </Menu>
-                                    </div>
+                                    ))}
                                 </div>
                             </div>
-                        </div>
 
-                        {/* Menu panel for small screens */}
-                        <Disclosure.Panel className="md:hidden">
-                            <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                                {navigation.map((item) => (
-                                    <NavLink
-                                        key={item.name}
-                                        to={item.to}
-                                        className="bg--900 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:bg-teal-700 hover:text-white focus:bg-gray-800 focus:text-white"
-                                    >
-                                        {item.name}
-                                    </NavLink>
-                                ))}
-                            </div>
-                            <div className="pt-4 pb-3 border-t border-gray-700">
-                                <div className="flex items-center px-5 sm:px-6">
-                                    <div className="flex-shrink-0">
-                                        <img className="h-10 w-10 rounded-full" src={user.imageUrl} alt="" />
-                                    </div>
-                                    <div className="ml-3">
-                                        <div className="text-base font-medium text-white">{user.name}</div>
-                                        <div className="text-sm font-medium text-gray-400">{user.email}</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </Disclosure.Panel>
-                    </>
-                )}
-            </Disclosure>
-        )
-    } else {
-        return (
-            <Disclosure as="nav" className="bg-teal-900">
-                {({ open }) => (
-                    <>
-                        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                            <div className="flex justify-between h-16">
-                                <div className="flex">
-                                    <div className="-ml-2 mr-2 flex items-center md:hidden">
-                                        {/* Mobile menu button */}
-                                        <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                                            <span className="sr-only">Open main menu</span>
-                                            {open ? (
-                                                <XIcon className="block h-6 w-6" aria-hidden="true" />
-                                            ) : (
-                                                <MenuIcon className="block h-6 w-6" aria-hidden="true" />
-                                            )}
-                                        </Disclosure.Button>
-                                    </div>
-                                    {/* Logo */}
-                                    <div className="flex-shrink-0 flex items-center">
-                                        <Link to='/'>
-                                            <img
-                                                className="h-20 w-auto"
-                                                src="https://user-images.githubusercontent.com/87889660/158719963-e835f37c-ef8f-4c1c-8c8b-e731aa073929.svg"
-                                                alt="Polari"
-                                            />
-
-                                        </Link>
-                                    </div>
-                                    <div className="hidden md:ml-6 md:flex md:items-center md:space-x-4">
-                                        {navigation.map((item) => (
-                                            <NavLink
-                                                key={item.name}
-                                                to={item.to}
-                                                className="px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:bg-teal-700 hover:text-white focus:bg-gray-800 focus:text-white"
+                            {Auth.loggedIn() ? (
+                                <>
+                                    <div className="flex items-center">
+                                        <div className="hidden md:mr-6 md:flex md:items-center md:space-x-4">
+                                            <button
+                                                key="logout"
+                                                onClick={logout}
+                                                className="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:bg-teal-700 hover:text-white focus:bg-gray-800 focus:text-white"
                                             >
-                                                {item.name}
+                                                Logout
+                                            </button>
+                                        </div>
+                                        <div className="flex-shrink-0">
+                                            <NavLink to='/messages' >
+                                                <button
+                                                    type="button"
+                                                    className="relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-teal-600 hover:bg-teal-500 focus:bg-teal-500 active:outline-none active:ring-2 active:ring-offset-2 active:ring-offset-gray-800 active:ring-teal-500"
+                                                >
+                                                    <ChatAltIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
+                                                    <span>Messages</span>
+                                                </button>
                                             </NavLink>
-                                        ))}
+                                        </div>
+                                        <div className="hidden md:ml-4 md:flex-shrink-0 md:flex md:items-center">
+                                            {/* Profile */}
+                                            <Menu as="div" className="ml-3 relative">
+                                                <div>
+                                                    <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                                                        <span className="sr-only">Open user menu</span>
+                                                        <img className="h-8 w-8 rounded-full" src={user.imageUrl} alt="" />
+                                                    </Menu.Button>
+                                                </div>
+                                            </Menu>
+                                        </div>
                                     </div>
-                                </div>
-
+                                </>
+                            ) : (
                                 <div className="flex items-center">
                                     <div className="flex-shrink-0">
                                         <div className="relative md:flex items-center justify-end md:flex-1 lg:w-0">
@@ -186,26 +130,62 @@ export default function Navbar() {
                                         </div>
                                     </div>
                                 </div>
-                            </div>
+                            )
+                            }
                         </div>
 
                         {/* Menu panel for small screens */}
-                        <Disclosure.Panel className="md:hidden">
-                            <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                                {navigation.map((item) => (
-                                    <NavLink
-                                        key={item.name}
-                                        to={item.to}
+                        {Auth.loggedIn() ? (
+                            <Disclosure.Panel className="md:hidden text-center">
+                                <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                                    {navigation.map((item) => (
+                                        <NavLink
+                                            key={item.name}
+                                            to={item.to}
+                                            className="bg--900 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:bg-teal-700 hover:text-white focus:bg-gray-800 focus:text-white"
+                                        >
+                                            {item.name}
+                                        </NavLink>
+                                    ))}
+                                    <a
+                                        key="logout"
+                                        onClick={logout}
                                         className="bg--900 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:bg-teal-700 hover:text-white focus:bg-gray-800 focus:text-white"
                                     >
-                                        {item.name}
-                                    </NavLink>
-                                ))}
-                            </div>
-                        </Disclosure.Panel>
-                    </>
-                )}
-            </Disclosure>
-        )
-    }
+                                        Logout
+                                    </a>
+                                </div>
+                                <div className="pt-4 pb-3 border-t border-gray-700">
+                                    <div className="flex items-center px-5 sm:px-6">
+                                        <div className="flex-shrink-0">
+                                            <img className="h-10 w-10 rounded-full" src={user.imageUrl} alt="" />
+                                        </div>
+                                        <div className="ml-3">
+                                            <div className="text-base font-medium text-white">{user.name}</div>
+                                            <div className="text-sm font-medium text-gray-400">{user.email}</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Disclosure.Panel>
+                        ) : (
+                            <Disclosure.Panel className="md:hidden text-center">
+                                <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                                    {navigation.map((item) => (
+                                        <NavLink
+                                            key={item.name}
+                                            to={item.to}
+                                            className="bg--900 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:bg-teal-700 hover:text-white focus:bg-gray-800 focus:text-white"
+                                        >
+                                            {item.name}
+                                        </NavLink>
+                                    ))}
+                                </div>
+                            </Disclosure.Panel>
+                        )
+                        }
+                    </div>
+                </>
+            )}
+        </Disclosure>
+    )
 }
--- src/utils/query.js
+++ src/utils/query.js
@@ -13,7 +13,7 @@ export const QUERY_PROFILES = gql`
     query allProfiles {
         profiles {
             _id
-            firstname
+            firstName
             photo
             attachmentStyle
             genderIdentity
@@ -35,7 +35,7 @@ export const QUERY_SINGLE_PROFILE = gql`
     query singleProfile($profileId: ID!) {
         profile(profileId: $profileId) {
             _id
-            firstname
+            firstName
             photo
             attachmentStyle
             genderIdentity
